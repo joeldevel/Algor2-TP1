99d676e66ed74b376e44cf93a753cbdbc81f87c1
joeldevel <christianjoel.developer@gmail.com>
2020-11-16 00:11:19 -0500

    ERROR, need to compile with flags

--- dc.c
+++ dc.c
@@ -3,73 +3,25 @@
 #include <stdio.h>
 #include <math.h>
 
-
-void analizar_tokens(char*tokens[]) {
-  printf("entrando a analizar\n" );
-  int i = 0;
-  while (tokens[i]!=NULL) {
-     // printf("tokens[%d] = %s\n", i,tokens[i]);
-     struct calc_token token_devuelto;
-     char *tok = tokens[i];
-     if (calc_parse(tok, &token_devuelto)){
-       // printf("se ha parseado con exito!\n");
-       switch (token_devuelto.type) {
-         case TOK_NUM:
-         printf("es un numero %d\n",token_devuelto.value );
-         break;
-         case TOK_OPER:
-         printf("es un operador " );
-         // printf("token_devuelto.oper=%d\n",token_devuelto.oper );
-         switch (token_devuelto.value) {
-           case OP_ADD: printf("+\n"); break;
-           case OP_SUB: printf("+\n"); break;
-           case OP_MUL: printf("*\n"); break;
-           case OP_DIV: printf("/\n"); break;
-           case OP_POW: printf("^\n"); break;
-           case OP_LOG: printf("log\n"); break;
-           case OP_RAIZ: printf("sqrt\n"); break;
-           case OP_TERN: printf("?\n"); break;
-         }
-         break;
-         case TOK_LPAREN: printf("es parentesis izquierdo (\n" );break;
-         case TOK_RPAREN: printf("es parentesis derecho )\n" );break;
-       }
-     }
-     i++;
-  }
-}
-// recibe un array de string con cada operando de la operacion
 void procesar_linea(char*tokens[]) {
-  // printf("entrando a procesar_linea\n" );
   int i = 0;
   pilanum_t *pila = pilanum_crear();
   while (tokens[i]!=NULL) {
      struct calc_token token_devuelto;
      char *tok = tokens[i];
      if (calc_parse(tok, &token_devuelto)){
-       // printf("se ha parseado con exito!\n");
        switch (token_devuelto.type) {
            case TOK_NUM:
-               // printf("Poner en la pila %d\n",token_devuelto.value );
-               // printf("sumandole 1 %d\n",token_devuelto.value+1 );
-               // if(pila_esta_vacia(pila))
-                // printf("La pila esta vacia\n");
-               // if (token_devuelto.value <0)
-                  // printf("el numero es negativo\n");
                apilar_num(pila, token_devuelto.value);
-               // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
            break;
            case TOK_OPER:
-               // printf("es un operador " );
-               // printf("token_devuelto.oper=%d\n",token_devuelto.oper );
+
                switch (token_devuelto.value) {
                    case OP_ADD:;
-                      // printf("Sumando +\n");
                       calc_num b = 0;
                       if(desapilar_num(pila, &b)){
                         printf("");
                       } else {
-                        // printf("no se desapila!!!\n");
                         printf("ERROR al desapilar suma\n");
                         break;
                       }
@@ -81,130 +33,88 @@ void procesar_linea(char*tokens[]) {
                         printf("ERROR al desapilar suma\n");
                         break;
                       }
-                      // printf("b=%p\n",b );
-                      // printf("b=%lu\n", b);
                       apilar_num(pila, (long int)a+(long int)b);
-                      // printf("Se apila =%d\n", (long int)a+(long int)b);
-                      // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
-
                       break;
                    case OP_SUB:
-                       // printf("restando -\n");
                        b = 0;
                        if(desapilar_num(pila, &b)){
-                         // printf("Desapilando\n");
                        } else {printf("no se desapila!!!\n");}
 
                        a = 0;
                        if(desapilar_num(pila, &a))
-                       // printf("Desapilando\n");
                        apilar_num(pila, (long int)a-(long int)b);
-                       // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
                        break;
                    case OP_MUL:
-                       // printf("Multiplicando\n");
                        b = 0;
                        if(desapilar_num(pila, &b)){
-                         // printf("Desapilando\n");
                        } else {printf("no se desapila!!!\n");}
 
                        a = 0;
                        if(desapilar_num(pila, &a))
-                       // printf("Desapilando\n");
                        apilar_num(pila, (long int)a*(long int)b);
-                       // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
                        break;
                    case OP_DIV:
-                       // printf("dividiendo\n");
                        b = 0;
                        if(desapilar_num(pila, &b)){
-                         // printf("Desapilando\n");
                        } else {printf("no se desapila!!!\n");}
 
                        a = 0;
                        if (desapilar_num(pila, &a))
-                       // printf("Desapilando\n");
                        if (b==0) {
                           printf("ERROR división por cero!!!\n");
                           break;
                         }
                        apilar_num(pila, (long int)a/(long int)b);
-                       // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
                        break;
-                      // printf("Division por cero /\n");
-                      // break;
                    case OP_POW:
-                       // printf("potencia\n");
                        b = 0;
                        if(desapilar_num(pila, &b)){
-                         // printf("Desapilando\n");
                        } else {printf("no se desapila!!!\n");}
 
                        a = 0;
                        if (desapilar_num(pila, &a))
-                        // printf("Desapilando\n");
                        if (b<0) {
                           printf("ERROR potencia negativa!!!\n");
                           break;
                         }
                        apilar_num(pila, (long int)pow(a,b));
-                       // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
                        break;
                    case OP_LOG:
                        printf("Longarismo\n");
-                       // base
                        b = 0;
                        if(desapilar_num(pila, &b)){
-                         // printf("Desapilando\n");
                        } else {printf("no se desapila!!!\n");}
-                       // numero a aplicar
                        a = 0;
                        if (desapilar_num(pila, &a))
-                        // printf("Desapilando\n");
                        if (b<2) {
                           printf("ERROR base menor a 2 !!!\n");
                           break;
                         }
                        apilar_num(pila, (long int)(log(a)/log(b)));
-                       // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
                        break;
-
                    case OP_RAIZ:
-                       // printf("raiz cuadrada\n");
-                       // necesito 1 solo operando
                        a = 0;
                        if (desapilar_num(pila, &a))
-                        // printf("Desapilando\n");
                        if (a<0) {
                           printf("ERROR raiz negativa!!!\n");
                           break;
                         }
                        apilar_num(pila, (long int)sqrt(a));
-                       // printf("ver tope pila =%lu\n", *(size_t*)pila_ver_tope(pila));
                        break;
                    case OP_TERN:
-                   // printf("op ternario\n");
-                      // op3
                       a = 0;
                       if(desapilar_num(pila, &a)){
-                        // printf("Desapilando\n");
                       } else {printf("no se desapila!!!\n");}
-                      // op2
                       b = 0;
                       if (desapilar_num(pila, &b)) {
-                        // printf("Desapilando\n");
                         }
                       calc_num c = 0;
                       if (!desapilar_num(pila, &c)) {
                          printf("ERROR falta un operando !!!\n");
                          break;
                        }
-                      // printf("a=%lu, b=%ld, c=%lu\n",a,b,c );
                       apilar_num(pila, (long int)(c?b:a));
-                      // printf("ver tope pila =%ld\n", *(size_t*)pila_ver_tope(pila));
                       break;
-
-                    // printf("recibe tres operandos?\n"); break;
                } break;
            case TOK_LPAREN: printf("es parentesis izquierdo (\n" );break;
            case TOK_RPAREN: printf("es parentesis derecho )\n" );break;
@@ -215,75 +125,17 @@ void procesar_linea(char*tokens[]) {
   }
   if (!pila_esta_vacia(pila)) {
     printf("%ld\n", *(size_t*)pila_ver_tope(pila));
-    // printf("ERROR pila vacia\n");
   }
-  // else {
-  // }
 
   pilanum_destruir(pila);
 }
 int main(int argc, char *argv[]) {
-  // printf("dc compila!\n");
-  // pilanum_t *pila = pilanum_crear();
-  // apilar_num(pila,3456);
-  // pilanum_destruir(pila);
-  // for( int i = 0; i < argc; i++) {
-  //   printf("argv[%i]= %s\n",i,argv[i] );
-  // }
-  // leer de un archivo
-  int ch = 0;
-  char *file_name = "ej.txt";
-  FILE* fh=fopen("ej.txt", "r");
-  if (!fh) {
-    printf("ERROR al abrir el archivo\n"); return 1;
-  }
-  else {
     char line[256];
     char **palabras = NULL;
     while (fgets(line, sizeof(line), stdin)) {
-        /* note that fgets don't strip the terminating \n, checking its
-           presence would allow to handle lines longer that sizeof(line) */
-        // printf("%s", line);
         palabras = dc_split(line);
-        // analizar_tokens(palabras);
         procesar_linea(palabras);
         free_strv(palabras);
     }
-  }
-  // int operandos[2] = {0,0};
-  // int i = 0;
-  // mientras no fin de archivo
-  // while ((ch = fgetc(fh)) != EOF) {
-  //   // mientras no fin de linea
-  //   while(ch != '\n') {
-  //     // si char no es operando ni operacion, seguir
-  //     if (ch == ' ') {
-  //       ch = fgetc(fh); printf(" espacio\n");
-  //       continue;
-  //     }
-  //     if (ch == '*' || ch == '+' || ch == '-' || ch == '/') {
-  //       putchar(ch);
-  //       printf(" es operador\n");
-  //     }
-  //     else {
-  //       putchar(ch);
-  //       operandos[i] = (int)ch - 48;
-  //       i++;
-  //       printf(" Es operando\n");
-  //     }
-  //     ch = fgetc(fh);
-  //   }
-  //   if (ch == '\n') printf("\n");
-  //   // else
-  //     // printf("%c",(char)ch);
-  //   // }
-  // }
-  // printf("operandos[0]=%d\n", operandos[0]);
-  // printf("operandos[1]=%d\n", operandos[1]);
-
-  // char **palabras = dc_split("123 9 + sqrt     log * ^ ( )");
-  // analizar_tokens(palabras);
-  // free_strv(palabras);
-  fclose(fh);
   return 0;
 }
--- strutil.c
+++ strutil.c
@@ -1,5 +1,4 @@
 #include "strutil.h"
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -8,7 +7,6 @@ char *substr(const char *str, size_t n) {
     if (!nueva_cadena) return NULL;                   // O(1)
     size_t i = 0;                                     // O(1)
     while (i < n) {
-      // printf("Dentro del while\n" );
       nueva_cadena[i] = str[i];
       i++;
     }                                                  // O(n)
@@ -16,26 +14,15 @@ char *substr(const char *str, size_t n) {
     return nueva_cadena;                               // O(1)
 }
 // char **split(const char *str, char sep) {
-char ** split_(const char *str, char sep) {
+char ** split(const char *str, char sep) {
     int string_ini = 0;
     int cantidad_strings = 0;
     int pos = 0;
     int contador_palabras = 0;
-    // while (str[pos]!='\0') {
-    //     if (str[pos]==sep) {
-    //         cantidad_strings++;
-    //         // printf("%c en pos %d\n", str[pos], pos);
-    //     } else {
-    //
-    //     }
-    //     pos++;
-    // }
-    // para saber cuantos elementos tiene el array de strings
     while(str[pos]!='\0') {
       if (str[pos]==sep) {
         cantidad_strings++;
       }
-      // printf("en el while\n");
       pos++;
     }     //O(n)
 
@@ -45,7 +32,6 @@ char ** split_(const char *str, char sep) {
     pos = 0;
     while(str[pos]!='\0') {
       if (str[pos]==sep) {
-        printf("inicio=%d, fin=%d\n",string_ini, pos);
         char *palabra = malloc(sizeof(char)*(pos-string_ini+1));
         memcpy(palabra, str+string_ini, pos-string_ini);
         palabra[pos-string_ini] = '\0';
@@ -56,76 +42,45 @@ char ** split_(const char *str, char sep) {
       pos++;
     }    //O(n)
     //copiar la ultima palabra
-    // printf("inicio=%d, fin=%d\n",string_ini, pos);
-
     char *palabra = malloc(sizeof(char)*(pos-string_ini+1));
     memcpy(palabra, str+string_ini, pos-string_ini);
     palabra[pos-string_ini] = '\0';
     palabras[contador_palabras] = palabra;
     contador_palabras++;
     palabras[contador_palabras] = NULL;
-    // char *palabra = malloc(sizeof(char)*(pos+1));
-    // memcpy(palabra, str, pos);
-    // palabra[pos] = '\0';
-    // printf("nueva palabra=%s\n",palabra);
-    // free(palabra);
-    printf("cantidad de nuevos strings = %d\n", cantidad_strings);
-    printf("tam palabras=%d\n", sizeof(palabras));
-    printf("tam char*=%d\n", sizeof(char*));
-    printf("strlen(str)=%d\n", strlen(str));
-    // printf("tam char=%d\n", sizeof(char));
     return palabras;
 }
-// char *join(char **strv, char sep);
-char *join_(char **strv, char sep) {
+char *join(char **strv, char sep) {
   int i = 0;
   int cantidad_letras = 0;
   while (strv[i]!=NULL) {
-     printf("palabras[%d] = %s\n", i, strv[i]);
      cantidad_letras+=strlen(strv[i])+1;
      i++;
   }
-  printf("cantidad de letras = %d\n", cantidad_letras );
   char *palabra = malloc(sizeof(char)*(cantidad_letras));
   i=0;
   int desplazamiento = 0;
   while (strv[i]!=NULL) {
-     // printf("palabras[%d] = %s\n", i,strv[i]);
      if ( strlen(strv[i]) == 0) {
-       printf("antes del continue\n");
        i++;
        continue;
      }
 
      if ( desplazamiento == 0 ){
-       printf("strlen(strv[%d])=%d, \n",i, strlen(strv[i]));
        memcpy(palabra+desplazamiento, strv[i], strlen(strv[i]));
        desplazamiento += strlen(strv[i]);
      }else {
        desplazamiento++;
-       printf("strlen(strv[%d])=%d, \n",i, strlen(strv[i]));
        memcpy(palabra+desplazamiento, strv[i], strlen(strv[i]));
        desplazamiento += strlen(strv[i]);
      }
-     // desplazamiento++;
-     // memcpy(palabra+desplazamiento+1, &sep, 1);
-     printf("desplazamiento=%d\n", desplazamiento);
-     printf("sep=%c\n", sep);
-     // palabra = sep;
-     // strcpy(palabra[desplazamiento],'#');
      palabra[desplazamiento] = sep;
-     // printf("strlen(strv[i])=%d, \n", strlen(strv[i]));
-     // printf("palabra[1]=%p, \n", palabra[1]);
-     // printf("palabra=%p, palabra[desplazamiento]=%p\n", palabra,&palabra[desplazamiento]);
-     // printf("palabra[desplazamiento]=%c\n", palabra[desplazamiento-1]);
-     // printf("palabra[desplazamiento]=%c\n", palabra[desplazamiento]);
-     // cantidad_letras+=strlen(strv[i])+1;
+
      i++;
   }
   palabra[desplazamiento] = '\0';
   return palabra;
 
-  // pedir memoria
 }
 void free_strv(char *strv[]) {
     int i = 0;
@@ -135,63 +90,3 @@ void free_strv(char *strv[]) {
     }
     free(strv);
 }
-// int main(int argc, char* argv[]) {
-//     printf("compila\n");
-//     char ** palabras = split_("pirulo, pirulas, chingolo, monchito", ',');
-//     // char ** palabras = split_("abc,def,ghi,jk", ',');
-//     // Casos borde propuestos
-//     // char ** palabras = split_("abc,,def", ',');
-//     // char ** palabras = split_("abc,def,", ',');
-//     // char ** palabras = split_(",abc,def", ',');
-//     // char ** palabras = split_("abc", '\0');
-//     // char ** palabras = split_("", ',');
-//     // char ** palabras = split_(",", ',');
-//
-//
-//     // split_("", ',');
-//     // char * palabra_original = "potirolis";
-//     // char * palabra_copiada = strdup(palabra_original);
-//     // printf("palabra original= %p, palabra_copiada=%p\n", palabra_original, palabra_copiada);
-//     // printf("palabra_original=%s, palabra_copiada=%s\n", palabra_original, palabra_copiada);
-//     // free(palabra_copiada);
-//     int i = 0;
-//     // while (palabras[i]!=NULL) {
-//     //    printf("palabras[%d] = %s\n", i,palabras[i]);
-//     //    i++;
-//     // }
-//     // i = 0;
-//     char *p = join_(palabras, ',');
-//     // p[0]='X';
-//     printf("la palabra es =%s\n",p );
-//     // while (palabras[i]!=NULL) {
-//     //    // printf("palabras[i] = %s\n", palabras[i]);
-//     //     free(palabras[i]);
-//     //    i++;
-//     // }
-//     // borrar la ultima?
-//     // free(palabras[i]);
-//     free(p);
-//     // free(palabras);
-//     free_strv(palabras);
-//     return 0;
-// }
-// int main(int argc, char *argv[]) {
-//
-//   printf("compila!!!\n");
-//   char * palabra = "Hola mundo";
-//   printf("palabra es = %s\n", palabra);
-//   char * sub_palabra = substr(palabra, 6);
-//   printf("la sub palabra es %s\n", sub_palabra);
-//   free(sub_palabra);
-//
-//   const char* ejemplo= "Ejemplo";
-//   sub_palabra = substr(ejemplo+4, 2);
-//   printf("la sub palabra es %s\n", sub_palabra);
-//   free(sub_palabra);
-//
-//   char * palabra2 = "Algoritmos";
-//   sub_palabra = substr(palabra2, 30);
-//   printf("la sub palabra es %s\n", sub_palabra);
-//   free(sub_palabra);
-//   return 0;
-// }
